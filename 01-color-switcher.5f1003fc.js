720c36cb9587def98c9fd04d7571c1f2b771ed67
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-06-30 15:13:26 +0000

    Deploying to gh-pages from @ Arkadiusz-Kruk/goit-js-hw-09@4d6e91cce51d7556f3f9c2b4f72e5e2ef883c16c 🚀

--- 01-color-switcher.5f1003fc.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),o=document.querySelector("body");function n(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,o.style.backgroundColor=n(),changeColor=setInterval((function(){var e=n();o.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(function(){clearInterval(changeColor),e.disabled=!1,t.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.5f1003fc.js.map
